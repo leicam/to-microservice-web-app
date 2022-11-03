2962a2b75ae4189745e436b4bf2b7dadfc732403
leicam <julianoleicam@gmail.com>
2022-11-03 17:35:34 -0300

    Tentativa de implementação de loading box

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { PoNotificationService } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent {
+  isHideLoading = true;
 
-  ngOnInit(): void {
+  constructor(private poNotification: PoNotificationService) {}
+
+  connectionTest() {
+    const message = 'Analysis ok';
+
+    this.isHideLoading = false;
+
+    setTimeout(() => {
+      this.isHideLoading = true;
+      this.poNotification.success(message);
+    }, 3000);
   }
-
 }
