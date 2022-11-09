09f555e793f4dea0c4fc1e29747240322fe41e90
leicam <julianoleicam@gmail.com>
2022-11-09 16:29:42 -0300

    Ajuste de mocks

--- src/app/interaction-graph-micro-solutions/interaction-graph-micro-solutions/interaction-graph-micro-solutions.component.ts
+++ src/app/interaction-graph-micro-solutions/interaction-graph-micro-solutions/interaction-graph-micro-solutions.component.ts
@@ -20,6 +20,69 @@ export class InteractionGraphMicroSolutionsComponent implements OnInit{
 
   constructor(private router: Router) { }
 
+  arrayMicroSolutions: Array<MicroSolutionsTable> = [
+    {
+      microService: 'microservice0',
+      metric: 'OverheadMaxPerMicroserviceArchitecture',
+      value: '240.0',
+      trace: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
+    },
+    {
+      microService: 'microservice1',
+      metric: 'FunctionalityPerMicroserviceArchitecture',
+      value: '3.041',
+      trace: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
+    },
+    {
+      microService: 'microservice2',
+      metric: 'OverheadMaxPerMicroserviceArchitecture',
+      value: '240.0',
+      trace: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
+    },
+    {
+      microService: 'microservice3',
+      metric: 'FunctionalityPerMicroserviceArchitecture',
+      value: '3.041',
+      trace: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
+    },
+    {
+      microService: 'microservice4',
+      metric: 'OverheadMaxPerMicroserviceArchitecture',
+      value: '240.0',
+      trace: 'teste.gerado.ManterLivro#Method:executar1#SizeOf:30#Deep:2#Thread:main'
+    },
+    {
+      microService: 'microservice5',
+      metric: 'OverheadMaxPerMicroserviceArchitecture',
+      value: '240.0',
+      trace: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
+    },
+    {
+      microService: 'microservice6',
+      metric: 'FunctionalityPerMicroserviceArchitecture',
+      value: '3.041',
+      trace: 'teste.gerado.ManterLivro#Method:executar1#SizeOf:30#Deep:2#Thread:main'
+    },
+    {
+      microService: 'microservice7',
+      metric: 'FunctionalityPerMicroserviceArchitecture',
+      value: '3.041',
+      trace: 'teste.gerado.ManterLivro#Method:executar1#SizeOf:30#Deep:2#Thread:main'
+    },
+    {
+      microService: 'microservice8',
+      metric: 'OverheadMaxPerMicroserviceArchitecture',
+      value: '240.0',
+      trace: 'teste.gerado.ManterLivro#Method:executar#SizeOf:30#Deep:1#Thread:main'
+    },
+    {
+      microService: 'microservice9',
+      metric: 'FunctionalityPerMicroserviceArchitecture',
+      value: '3.041',
+      trace: 'teste.gerado.ManterLivro#Method:executar1#SizeOf:30#Deep:2#Thread:main'
+    },
+  ];
+
   ngOnInit(): void {
     this.items = [
       {
@@ -37,16 +100,16 @@ export class InteractionGraphMicroSolutionsComponent implements OnInit{
         label: 'Microservice 1',
         items: [
           {
-            label: 'Microservice 1'
-          },
-          {
             label: 'Microservice 2'
           },
           {
-            label: 'Microservice 3'
+            label: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
           },
           {
-            label: 'Microservice 4'
+            label: 'teste.gerado.ManterLivro#Method:executar1#SizeOf:30#Deep:2#Thread:main'
+          },
+          {
+            label: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
           }
         ]
       },
@@ -54,16 +117,16 @@ export class InteractionGraphMicroSolutionsComponent implements OnInit{
         label: 'Microservice 2',
         items: [
           {
-            label: 'Microservice 1'
+            label: 'test.generate.KeepStudentOne.executeFour!KeepStudent!'
           },
           {
-            label: 'Microservice 2'
+            label: 'teste.gerado.ManterAluno#Method:executar#SizeOf:30#Deep:1#Thread:main'
           },
           {
-            label: 'Microservice 3'
+            label: 'Microservice 0'
           },
           {
-            label: 'Microservice 4'
+            label: 'teste.gerado.ManterLivro#Method:executar1#SizeOf:30#Deep:2#Thread:main'
           }
         ]
       }
